2b14ec703a0346d0c49f562b4574362641446844
Nehamindsnxt <Neham.mindsnxt.com>
2025-11-17 19:39:07 +0530

    latest code

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
 function toggleMobileMenu() {
     const navLinks = document.querySelector('.nav-links');
     navLinks.classList.toggle('active');
